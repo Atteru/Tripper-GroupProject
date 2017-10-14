1e0f1f0c53511d7b3ad68e634057923fb8abb34f
michalm2811994 <michalm2811994@interia.pl>
2017-10-15 00:23:58 +0200

    Zmiany w bazie + Inserty do tabel
    Potestowałem sobie trochę dzisiaj naszą baze i zdecydowałem się znacznie
    uprościć wpowadzanie informacji o transporcie. Usunąłem tabele z
    biletami a wszystkie jej kolumny przeniosłem do tabeli transport. Opcja
    dodaj bilet będzie w kodzie programu (wtedy prosimy o podanie większej
    ilości informacji) ale dane zacjągają się z tej samej tabeli która
    obsłuje podróż własnym autem, gdzie będą nullami albo domyślnymi
    wartościami.
    Pozmieniałem trochę tabele Country, zmieniając klucz z inta na chara(3),
    dodałem kolumne z kontynentami i uzupełniłem już całą tabelę insertami.
    Do pozostałych tabel też zrobiłem kilka insertów tak by łatwiej
    testowało się program

--- SQLQuery Views.sql
+++ SQLQuery Views.sql
@@ -8,7 +8,7 @@ IF EXISTS(SELECT name FROM sys.views
 GO
 
 CREATE VIEW v_Localization AS
-SELECT l.LocalizationID, l.City, c.CountryID ,c.name AS Country FROM Localization AS l
+SELECT l.LocalizationID, l.City, c.CountryID ,c.name AS Country, c.Continent FROM Localization AS l
 JOIN Country AS c ON c.CountryID = l.CountryID;
 GO
 
@@ -19,33 +19,23 @@ IF EXISTS(SELECT name FROM sys.views
 GO
 
 CREATE VIEW v_Transport AS
-SELECT t.TransportID, t.Cost, t.IsReturn, t.TripID, tp.Name AS TripName, v.Name AS Vehicle FROM Transport AS T
+SELECT t.TransportID, t.TripID, t.IsReturn, t.Vehicle, tp.Name AS TripName, t.Cost, t.Seats, t.DepartureTime, t.ArrivalTime, cd.Name AS DepartreCountry, ld .City AS DepartureCity, ca.Name AS ArrivalCountry, la.City AS ArrivalCity FROM Transport AS t
 JOIN Trip AS tp on t.TripID = tp.TripID
-JOIN Vehicle AS v on t.VehicleID = v.VehicleID;
-GO
-
-
-IF EXISTS(SELECT name FROM sys.views
-    WHERE name = 'v_Ticket')
-    DROP VIEW v_Ticket;
-GO
-
-
-CREATE VIEW v_Ticket AS
-SELECT t.TicketID, t.Seats, t.Departure_Time, t.ArrivalTime, t.TransportID, cd.Name AS DepartreCountry, ld .City AS DepartureCity, ca.Name AS ArrivalCountry, la.City AS ArrivalCity  FROM Ticket AS t
 JOIN Localization AS ld ON t.DepartureLocalization = ld.LocalizationID
 JOIN Country AS cd ON cd.CountryID = ld.CountryID
 JOIN Localization AS la ON t.ArrivalLocalization = la.LocalizationID
 JOIN Country AS ca ON ca.CountryID = la.CountryID;
 GO
+GO
+
 
 IF EXISTS(SELECT name FROM sys.views
     WHERE name = 'v_Stayment')
     DROP VIEW v_Stayment;
 GO
 
-
-SELECT s.StaymentID, s.Name, s.DateFrom, s.DateTo, s.TripID, c.Name AS Country, l.City AS City FROM Stayment AS s
+CREATE VIEW v_Stayment AS
+SELECT s.StaymentID, s.Address, s.DateFrom, s.DateTo, s.TripID, c.Name AS Country, l.City AS City FROM Stayment AS s
 JOIN Localization AS l ON s.LocalizationID = l.LocalizationID
 JOIN Country AS c ON c.CountryID = l.CountryID;
 GO
