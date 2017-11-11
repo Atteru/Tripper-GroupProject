eb470925d236bfc6c57ba554ad8f080b27f80afe
Wojciech Zieliński <ateru01@gmail.com>
2017-11-11 19:20:24 +0100

    Database Added

---
+++ DbLogic/DbConnection.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tripper.DbLogic.LinqToSQL;
+
+namespace Tripper.DbLogic
+{
+    public sealed class Connection
+    {
+
+        // Instancja klasy TripperDataDataContext - Connection.TriperData pozwala na odwoływanie się do obiektów bazy dalnych za pomocą metod LINQ
+        // Zrealizowana w formie singletona 
+
+        private static TripperDataDataContext _tripperData = null;
+        private static readonly object _PadLock = new object();
+
+        public static TripperDataDataContext TripperData
+        {
+            get
+            {
+                lock (_PadLock)
+                {
+                    if (_tripperData == null)
+                    {
+                        _tripperData = new TripperDataDataContext();
+                    }
+                    return _tripperData;
+                }
+            }
+        }
+
+
+    }
+}
