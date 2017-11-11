977c0a792ea73c0427d6da49d33da4ecf89db4f1
Wojciech Zieliński <ateru01@gmail.com>
2017-11-11 19:28:46 +0100

    DataBase fixed

---
+++ Tripper/Enums/Vehicle.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tripper.Enums
+{
+    public enum Vehicle
+    {
+        Samolot, Samochód, Pociąg, Bus
+    }
+}
