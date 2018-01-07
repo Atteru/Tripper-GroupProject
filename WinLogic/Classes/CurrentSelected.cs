c431f4cc91f0c3f60441ceadb1d8531aa3b7a652
michalm2811994 <michalm2811994@interia.pl>
2018-01-07 21:04:21 +0100

    Revert "Revert "Tripper Release Candidate""
    This reverts commit a0b8293f165eae9cac230338355d2a6a00b84acc.

---
+++ WinLogic/Classes/CurrentSelected.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Tripper.DbLogic;
+using Tripper.DbLogic.LinqToSQL;
+using System.Linq.Expressions;
+using System.ComponentModel;
+
+namespace Tripper.WinLogic.Classes
+{
+    public static class CurrentSelected 
+    {
+        private static Trip _trip;
+        public static Trip Trip
+        {
+            get
+            {
+                return _trip;
+            }
+            set
+            {
+                _trip = value;
+            }
+        }
+
+        private static Traveler _traveler;
+        public static Traveler Traveler
+        {
+            get
+            {
+                return _traveler;
+            }
+            set
+            {
+                _traveler = value;
+            }
+        }
+
+    }
+}
---
+++ WinLogic/UserControls/NoteForm.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace Tripper.WinLogic.UserControls
+{
+    partial class NoteForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.bAdd = new System.Windows.Forms.Button();
+            this.bCancel = new System.Windows.Forms.Button();
+            this.lTitle = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.richTextBox = new System.Windows.Forms.RichTextBox();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.bAdd);
+            this.panel1.Controls.Add(this.bCancel);
+            this.panel1.Controls.Add(this.lTitle);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Padding = new System.Windows.Forms.Padding(5);
+            this.panel1.Size = new System.Drawing.Size(656, 43);
+            this.panel1.TabIndex = 11;
+            // 
+            // bAdd
+            // 
+            this.bAdd.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.bAdd.BackColor = System.Drawing.Color.Teal;
+            this.bAdd.FlatAppearance.BorderColor = System.Drawing.Color.DarkSlateGray;
+            this.bAdd.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.bAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.bAdd.ForeColor = System.Drawing.Color.White;
+            this.bAdd.Location = new System.Drawing.Point(400, 5);
+            this.bAdd.Margin = new System.Windows.Forms.Padding(0, 0, 5, 0);
+            this.bAdd.Name = "bAdd";
+            this.bAdd.Size = new System.Drawing.Size(124, 30);
+            this.bAdd.TabIndex = 168;
+            this.bAdd.Text = "Zapisz";
+            this.bAdd.UseVisualStyleBackColor = false;
+            this.bAdd.Click += new System.EventHandler(this.bAdd_Click);
+            // 
+            // bCancel
+            // 
+            this.bCancel.BackColor = System.Drawing.Color.Teal;
+            this.bCancel.Dock = System.Windows.Forms.DockStyle.Right;
+            this.bCancel.FlatAppearance.BorderColor = System.Drawing.Color.DarkSlateGray;
+            this.bCancel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.bCancel.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.bCancel.ForeColor = System.Drawing.Color.White;
+            this.bCancel.Location = new System.Drawing.Point(529, 5);
+            this.bCancel.Margin = new System.Windows.Forms.Padding(0, 0, 5, 0);
+            this.bCancel.Name = "bCancel";
+            this.bCancel.Size = new System.Drawing.Size(120, 31);
+            this.bCancel.TabIndex = 169;
+            this.bCancel.Text = "Anuluj";
+            this.bCancel.UseVisualStyleBackColor = false;
+            this.bCancel.Click += new System.EventHandler(this.bCancel_Click);
+            // 
+            // lTitle
+            // 
+            this.lTitle.AutoSize = true;
+            this.lTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lTitle.ForeColor = System.Drawing.Color.DarkSlateGray;
+            this.lTitle.Location = new System.Drawing.Point(25, 10);
+            this.lTitle.Margin = new System.Windows.Forms.Padding(0);
+            this.lTitle.Name = "lTitle";
+            this.lTitle.Size = new System.Drawing.Size(48, 20);
+            this.lTitle.TabIndex = 167;
+            this.lTitle.Text = "Tytuł";
+            // 
+            // panel2
+            // 
+            this.panel2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Controls.Add(this.richTextBox);
+            this.panel2.Location = new System.Drawing.Point(12, 61);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(656, 386);
+            this.panel2.TabIndex = 12;
+            // 
+            // richTextBox
+            // 
+            this.richTextBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.richTextBox.Location = new System.Drawing.Point(0, 0);
+            this.richTextBox.Name = "richTextBox";
+            this.richTextBox.Size = new System.Drawing.Size(654, 384);
+            this.richTextBox.TabIndex = 11;
+            this.richTextBox.Text = "";
+            // 
+            // NoteForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(680, 459);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MinimizeBox = false;
+            this.Name = "NoteForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.NoteForm_FormClosing);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button bAdd;
+        private System.Windows.Forms.Button bCancel;
+        private System.Windows.Forms.Label lTitle;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.RichTextBox richTextBox;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+    }
+}
